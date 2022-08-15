d682544e88036bf1b24f186bf67c23413a328cdd
yuhuashui <98299873+yuhuashui@users.noreply.github.com>
2022-08-15 22:52:28 +0800

    11

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ import (
 	//                               vvvv                               //
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ai_false" // 服务器监控
-	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/aiwife"        // 随机老婆
+	 _ "github.com/FloatTech/ZeroBot-Plugin/plugin/aiwife"        // 随机老婆
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/b14"           // base16384加解密
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/baidu"    // 百度一下
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bilibili" // b站相关
@@ -64,7 +64,7 @@ import (
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/cangtoushi"    // 藏头诗
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/char_reverser" // 英文字符翻转
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/choose"        // 选择困难症帮手
-	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chouxianghua"  // 说抽象话
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chouxianghua"  // 说抽象话
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/coser" // 三次元小姐姐
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/cpstory"       // cp短打
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/danbooru"     // DeepDanbooru二次元图标签识别
